13fd5b6e42cc4b69302ad452877b014a6df5afa9
EddieOrozco <orozcoe230@gmail.com>
2022-11-08 09:33:00 -0600

    Adding BroserRoter to index.js so react-router can work on page.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './bootstrap.min.css';
 import './index.css';
@@ -7,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
